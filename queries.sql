3c08a189fa0c00fbd70fb00306ae451e6cfb38df
Ramiro Gonzalez <ramirogonzalez@mercedenergy.com>
2019-11-14 13:03:52 -0800

    Updated

--- queries.sql
+++ queries.sql
@@ -47,4 +47,48 @@ WHERE e_name LIKE "California%"
 UPDATE calendar 
 SET cal_id = (SELECT e_id FROM events WHERE e_name LIKE 'Half%'),
     cal_date = (SELECT e_date FROM events WHERE e_name LIKE 'Half%')
-    
+/***********************3******************************/
+/*Orgs remove events */
+DELETE FROM events
+    WHERE e_id = 29082
+/***********************4******************************/
+/*Orgs push nofications */
+UPDATE notifications
+    SET
+    (n_id, n_type) = (SELECT o_id, o_name FROM organizations WHERE o_id = 48 )
+WHERE EXISTS(
+    SELECT *
+    FROM organizations AS T1, notifications as T2
+    WHERE T1.o_id = T2.n_id
+)
+/*********************5***************************/
+/*Organizations stock the store */ 
+
+
+INSERT INTO store (
+                      item_desc,
+                      item_name,
+                      item_cost,
+                      item_quantity,
+                      item_id
+                  )
+                  VALUES (
+                      (SELECT o_name 
+FROM organizations 
+WHERE o_name LIKE 'Basic%' ),
+                      'Flask',
+                      20,
+                      10,
+                      1
+                  );
+/***********************6***********************/
+/*Find the number of items in the inventory of Bobcat Pantry*/ 
+SELECT SUM(item_quantity)
+FROM store
+WHERE item_desc LIKE 'Bobcat Pantry%';
+
+/***********************7***********************/
+/*Student purchases item by filtering */
+SELECT item_name , item_
+FROM store 
+WHERE item_name LIKE 'Shirt%' 
